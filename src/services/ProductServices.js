0972f69ba9fd367934d0832310e27c4287aeefa4
hoangnhor <hoangtranvan999@gmail.com>
2025-04-22 04:10:05 +0700

    moi nhat

--- src/services/ProductServices.js
+++ src/services/ProductServices.js
@@ -2,24 +2,24 @@ import axios from "axios"
 import { axiosJWT } from "./UserServices"
 
 export const getAllProduct = async () => {
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/getall`)
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/product/getall`)
     return res.data
 }
 
 export const createProduct = async (data) => {
-    const res = await axios.post(`${process.env.REACT_APP_API_URL}/product/create`, data)
+    const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/product/create`, data)
     return res.data
 }
 export const getDetailsProduct = async (id) => {
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/get-details/${id}`)
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/product/get-details/${id}`)
     return res.data
 }
 export const searchProduct = async (keyword) => {
-    const res = await axios.get(`${process.env.REACT_APP_API_URL}/product/search?keyword=${encodeURIComponent(keyword)}`);
+    const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/product/search?keyword=${encodeURIComponent(keyword)}`);
     return res.data;
 };
 export const updateProduct = async (id, access_token, data) => {
-    const res = await axiosJWT.put(`${process.env.REACT_APP_API_URL}/product/update/${id}`, data, {
+    const res = await axiosJWT.put(`${process.env.REACT_APP_API_URL}/api/product/update/${id}`, data, {
         headers: {
             token: `Bearer ${access_token}`,
         }
